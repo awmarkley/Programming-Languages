9e98e3edba038a6d587e16bb7c32b36be08674ff
Andrew Markley <amarkley@uab.edu>
2017-02-08 14:53:21 -0600

    Added tail recursion

--- Scheme/a01.scm
+++ Scheme/a01.scm
@@ -1,60 +1,69 @@
-#lang racket
+;;; Boniface Sindala and Andrew Markley
+;;; CS401
+;;; Assignment 1
 
 (define (s-exp exp)
+  (let (( x (eval exp)))
+    (if (equal? exp x )
+        exp
+        (s-exp x)
+    )
+  )
+)
+
+(define (eval exp)
   (cond
-    ;;Fully simplified
-    ((number? exp) exp)
+      ;;Fully simplified
+      ((number? exp) exp)
 
-    ;;Undefined variable
-    ((symbol? exp) exp)
+      ;;Undefined variable
+      ((symbol? exp) exp)
 
-    ;;Nested summation, second sum at index 2
-    ((and (sum? exp) (> (length exp) 2) (sum? (caddr exp)))
-     (s-sum3 (cadr exp) (cadr (caddr exp)) (caddr (caddr exp))))
+      ;;Nested summation, second sum at index 2
+      ((and (sum? exp) (> (length exp) 2) (sum? (caddr exp)) (not (pair? (cadr exp))))
+       (s-sum3 (cadr exp) (cadr (caddr exp)) (caddr (caddr exp))))
 
-    ;;Nested summation, second sum at index 1
-    ((and (sum? exp) (> (length exp) 2) (sum? (cadr exp)))
-     (s-sum3 (cadr (cadr exp)) (caddr (cadr exp)) (caddr exp)))
+      ;;Nested summation, second sum at index 1
+      ((and (sum? exp) (> (length exp) 2) (sum? (cadr exp)) (not (pair? (caddr exp))))
+       (s-sum3 (cadr (cadr exp)) (caddr (cadr exp)) (caddr exp)))
 
-    ;;Nested multiplication, second mul at index 2
-    ((and (mul? exp) (> (length exp) 2) (mul? (caddr exp)))
-     (s-mul3 (cadr exp) (cadr (caddr exp)) (caddr (caddr exp))))
+      ;;Nested multiplication, second mul at index 2
+      ((and (mul? exp) (> (length exp) 2) (mul? (caddr exp)) (not (pair? (cadr exp))))
+       (s-mul3 (cadr exp) (cadr (caddr exp)) (caddr (caddr exp))))
 
-    ;;Nested multiplication, second mul at index 1
-    ((and (mul? exp) (> (length exp) 2) (mul? (cadr exp)))
-     (s-mul3 (cadr (cadr exp)) (caddr (cadr exp)) (caddr exp)))
+      ;;Nested multiplication, second mul at index 1
+      ((and (mul? exp) (> (length exp) 2) (mul? (cadr exp)) (not (pair? (caddr exp))))
+       (s-mul3 (cadr (cadr exp)) (caddr (cadr exp)) (caddr exp)))
 
-    ;;Nested mul with sum at index 1
-    ((and (mul? exp) (sum? (cadr exp)))
-     (s-mul-sum (caddr exp) (cadr exp)))
+      ;;Nested mul with sum at index 1
+      ((and (mul? exp) (sum? (cadr exp)))
+       (s-mul-sum (caddr exp) (cadr exp)))
 
-    ;;Nested mul with sum at index 2
-    ((and (mul? exp) (sum? (caddr exp)))
-     (s-mul-sum (cadr exp) (caddr exp)))
+      ;;Nested mul with sum at index 2
+      ((and (mul? exp) (sum? (caddr exp)))
+       (s-mul-sum (cadr exp) (caddr exp)))
 
-    ;;Nested mul with sub at index 1
-    ((and (mul? exp) (sub? (cadr exp)))
-     (s-mul-sub (caddr exp) (cadr exp)))
+      ;;Nested mul with sub at index 1
+      ((and (mul? exp) (sub? (cadr exp)))
+       (s-mul-sub (caddr exp) (cadr exp)))
 
-    ;Nested mul with sub at index 2
-    ((and (mul? exp) (sub? (caddr exp)))
-     (s-mul-sub (cadr exp) (caddr exp)))
+      ;Nested mul with sub at index 2
+      ((and (mul? exp) (sub? (caddr exp)))
+       (s-mul-sub (cadr exp) (caddr exp)))
     
-    ;;Simple addition
-    ((sum? exp)
-     (s-sum (s-exp (cadr exp)) (s-exp (caddr exp))))
+      ;;Simple addition
+      ((and (sum? exp) (not (pair? (cadr exp))) (not (pair? (caddr exp))))
+       (s-sum (s-exp (cadr exp)) (s-exp (caddr exp))))
 
-    ;;Simple multiplication
-    ((mul? exp)
-     (s-mul (s-exp (cadr exp)) (s-exp (caddr exp))))
+      ;;Simple multiplication
+      ((and (mul? exp) (not (pair? (cadr exp))) (not (pair? (caddr exp))))
+       (s-mul (s-exp (cadr exp)) (s-exp (caddr exp))))
 
-    ;;Simple subtraction
-    ((sub? exp)
-     (s-minus (s-exp (cadr exp)) (s-exp (caddr exp))))
+      ;;Simple subtraction
+      ((and (sub? exp) (not (pair? (cadr exp))) (not (pair? (caddr exp))))
+       (s-minus (s-exp (cadr exp)) (s-exp (caddr exp))))
 
-
-
-    (else "Whoops! I don't understand this expression.")
+      (else exp)
     )
   )
 
@@ -111,14 +120,17 @@
     ((and (number? x) (number? y)) (- x y))
     ((and (null? y) (number? x)) (negate x))
     ((and (not (number? x)) (number? y)) (list '+ (negate y) x))
-    ((and (number? x) (symbol? y)) (list '+ '(- y) x))
+    ((and (number? x) (symbol? y)) (list '+ (list '- y) x))
     (else (list '+ (negate y) x))
   )
 )
 
 (define (negate x)
-  (* -1 x)
+  (cond
+     ((number? x) (* -1 x))
+     (else (list '- x))
   )
+)
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+))
@@ -137,81 +149,81 @@
 ;;;(display " -> ")
 
 ;1
-(display "(+ 1 2) -> ")
-(s-exp '(+ 1 2))
+(display "\n(+ 1 2) -> ")
+(display (s-exp '(+ 1 2)))
 
 ;2
-(display "(+ a 2) -> ")
-(s-exp '(+ a 2))
+(display "\n(+ a 2) -> ")
+(display (s-exp '(+ a 2)))
 
 ;3
-(display "(* 2 3) -> ")
-(s-exp '(* 2 3))
+(display "\n(* 2 3) -> ")
+(display (s-exp '(* 2 3)))
 
 ;4
-(display "(* a 3) -> ")
-(s-exp '(* a 3))
+(display "\n(* a 3) -> ")
+(display (s-exp '(* a 3)))
 
 ;5
-(display "(- 3 2) -> ")
-(s-exp '(- 3 2))
+(display "\n(- 3 2) -> ")
+(display (s-exp '(- 3 2)))
 
 ;6
-(display "(- a 2) -> ")
-(s-exp '(- a 2))
+(display "\n(- a 2) -> ")
+(display (s-exp '(- a 2)))
 
 ;7
-(display "(+ a (+ b c)) -> ")
-(s-exp '(+ a (+ b c)))
+(display "\n(+ a (+ b c)) -> ")
+(display (s-exp '(+ a (+ b c))))
 
 ;8
-(display "(* a (* b c)) -> ")
-(s-exp '(* a (* b c)))
+(display "\n(* a (* b c)) -> ")
+(display (s-exp '(* a (* b c))))
 
 ;9
-(display "(+ (+ 1 a) 2) -> ")
-(s-exp '(+ (+ 1 a) 2))
+(display "\n(+ (+ 1 a) 2) -> ")
+(display (s-exp '(+ (+ 1 a) 2)))
 
 ;10
-(display "(* (* 2 a) 3) -> ")
-(s-exp '(* (* 2 a) 3))
+(display "\n(* (* 2 a) 3) -> ")
+(display (s-exp '(* (* 2 a) 3)))
 
 ;11
-(display "(* (+ 2 a) 3) -> ")
-(s-exp '(* (+ 2 a) 3))
+(display "\n(* (+ 2 a) 3) -> ")
+(display (s-exp '(* (+ 2 a) 3)))
 
 ;12
-(display "(* 2 (+ 3 a)) -> ")
-(s-exp '(* 2 (+ 3 a)))
+(display "\n(* 2 (+ 3 a)) -> ")
+(display (s-exp '(* 2 (+ 3 a))))
 
 ;13
-(display "(* (+ a b) 2)) -> ")
-(s-exp '(* (+ a b) 2))
+(display "\n(* (+ a b) 2)) -> ")
+(display (s-exp '(* (+ a b) 2)))
 
 ;14
-(display "(* 3 (+ a b)) -> ")
-(s-exp '(* 3 (+ a b)))
+(display "\n(* 3 (+ a b)) -> ")
+(display (s-exp '(* 3 (+ a b))))
 
 ;15
-(display "(* (- a b) 2)) -> ")
-(s-exp '(* (- a b) 2))
+(display "\n(* (- a b) 2)) -> ")
+(display (s-exp '(* (- a b) 2)))
 
 ;16
-(display "(* 2 (- a b)) -> ")
-(s-exp '(* 2 (- a b)))
+(display "\n(* 2 (- a b)) -> ")
+(display (s-exp '(* 2 (- a b))))
 
 ;17
-(display "(* (+ a b) c)) -> ")
-(s-exp '(* (+ a b) c))
+(display "\n(* (+ a b) c)) -> ")
+(display (s-exp '(* (+ a b) c)))
 
 ;18
-(display "(* a (+ b c)) -> ")
-(s-exp '(* a (+ b c)))
+(display "\n(* a (+ b c)) -> ")
+(display (s-exp '(* a (+ b c))))
 
 ;19
-(display "(* (- a b) c) -> ")
-(s-exp '(* (- a b) c))
+(display "\n(* (- a b) c) -> ")
+(display (s-exp '(* (- a b) c)))
 
 ;20
-(display "(* a (- b c)) -> ")
-(s-exp '(* a (- b c)))
+(display "\n(* a (- b c)) -> ")
+(display (s-exp '(* a (- b c))))
